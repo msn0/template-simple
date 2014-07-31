d92c94af476388ef04851bafa5458ce04a55b253
Michał Jezierski <ambinanitelo@gmail.com>
2014-07-31 11:05:42 +0200

    simple template - karma + jasmine

---
+++ spec/fake-spec.js
@@ -0,0 +1,11 @@
+describe("fake", function () {
+
+  beforeEach(function () {
+    this.fake = new Fake();
+  });
+
+  it("should be true", function () {
+    expect(true).toBeTruthy();
+  });
+
+});
---
+++ src/fake.js
@@ -0,0 +1,5 @@
+/* exported Fake */
+
+var Fake = function () {
+  "use strict";
+};
